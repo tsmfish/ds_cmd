3b5b32da47391ba1f475ac178d63c2169202d650
tsmfish <tsmfish@gmail.com>
2017-03-03 17:21:01 +0200

    Complete.

--- ds_cmd.py
+++ ds_cmd.py
@@ -1,37 +1,71 @@
 #!/usr/bin/env python2.6
 # -*- coding: utf-8
+
 import base64
 import getpass
 import optparse
 import random
 import threading
 import time
+import Queue
 
-from Queue import Queue
+from ds_helper import COLORS, print_for_ds, extract, is_contains
 
-from ds_helper import COLORS, print_for_ds, print_message_format, extract, is_contains
+import sys
+sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/ecdsa-0.13-py2.6.egg/')
+sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/requests-2.9.1-py2.6.egg')
+sys.path.insert(1, '/home/erkki/.local/lib/python2.6/site-packages/paramiko-1.16.0-py2.6.egg')
+
+from netmiko import ConnectHandler, NetMikoTimeoutException, NetMikoAuthenticationException
+
 
 log_file_format = "%y%m%d_%H%M%S_{ds_name}.log"
 
 
 COMPLETE, FATAL, TEMPORARY = 'complete', 'fatal', 'temporary'
-NAME, RESULT = 'name', 'result'
+NAME, RESULT, PRINTOUTS = 'name', 'result', 'printouts'
 
 
 RETRY_CONNECTION_LIMIT = 5
 FAIL_CONNECTION_WAIT_INTERVALS = [2, 3, 3, 7, 9, 13, 17, 25, 39]
 RANDOM_WAIT_TIME = 5
 
+ds_name_pattern = r"\b\w+?\d-\w+?\d{0,4}\b"
+comment_line_pattern = r"^\s*?[#/][^\n]+$"
 
-def update_ds(ds_name,
-              user,
-              password,
-              result_queue=Queue(),
-              io_lock=None,
-              force_delete=False,
-              log_to_file=False,
-              color=None,
-              force_load=False):
+
+def post_result(result, queue=None, log_file_name=None):
+    """
+
+    :param result: {NAME: name, RESULT: result}
+    :type result: dict 
+    :param queue: Queue object, where result posted
+    :type queue: Queue
+    :param log_file_name: 
+    :type log_file_name: str
+    :rtype: None
+    """
+    if queue:
+        queue.put(result)
+    if log_file_name:
+        try:
+            with open(log_file_name, 'a') as log_file:
+                log_file.write("[{0}] : ***** result: {1} *****\n"
+                               .format(result[NAME],
+                                       result[RESULT].upper()))
+                log_file.close()
+        except IOError:
+            pass
+
+
+def execute_commands(ds_name,
+                     user,
+                     password,
+                     commands,
+                     result_queue,
+                     io_lock=None,
+                     log_to_file=False,
+                     color=None):
     """
     
     Upgrade ds to new SW
@@ -42,18 +76,16 @@ def update_ds(ds_name,
     :type user: str
     :param password:
     :type password: str
+    :param commands: list with commands
+    :type commands: list
     :param result_queue:
     :type result_queue: Queue
     :param io_lock: 
     :type io_lock: Lock()
-    :param force_delete: 
-    :type force_delete: bool
     :param log_to_file: 
     :type log_to_file: str
     :param color: 
     :type color: COLORS
-    :param force_load: 
-    :type force_load: bool
     :rtype: None
     """
 
@@ -64,48 +96,60 @@ def update_ds(ds_name,
         log_file_name = None
 
     # Create object
-    node = DS(ds_name, user, password)
+    paramiters = {
+        'device_type': 'alcatel_sros',
+        'host': ds_name,
+        'port': 22,
+        'username': user,
+        'password': password,
+        'global_delay_factor': 1,
+        'ssh_strict': False,
+        'timeout': 8.0,
+    }
 
     # Connect and get basic inform
     print_for_ds(ds_name,
-                 '=' * 8 + ' Start process for {ds} '.format(ds=node.ip) + '=' * 8,
+                 '=' * 8 + ' Start process ...' + '=' * 8,
                  io_lock,
                  log_file_name,
                  color)
 
     for tray in range(RETRY_CONNECTION_LIMIT):
         try:
-            node.conn()
+            connection = ConnectHandler(paramiters)
             break
-        except ExceptionWrongPassword:
-            print_for_ds(ds_name, 'Wrong password', io_lock, log_file_name, color, COLORS.error)
-            post_result({NAME: ds_name, RESULT: FATAL}, result_queue, log_file_name)
-            return
-        except ExceptionHostUnreachable:
-            print_for_ds(ds_name, 'Cannot connect!', io_lock, log_file_name, color, COLORS.error)
-            post_result({NAME: ds_name, RESULT: FATAL}, result_queue, log_file_name)
-            return
-        except :
+        except NetMikoTimeoutException as e:
+            print_for_ds(ds_name, str(e))
+        except NetMikoAuthenticationException as e:
+            print_for_ds(ds_name, str(e))
+        except Exception as e:
             if tray != RETRY_CONNECTION_LIMIT - 1:
-                print_for_ds(ds_name, 'Cannot connect! Try reconnect...', io_lock, log_file_name, color)
+                print_for_ds(ds_name, 'Cannot connect! Try reconnect...', io_lock, log_file_name, color, COLORS.info)
             else:
                 print_for_ds(ds_name, 'Cannot connect!', io_lock, log_file_name, color, COLORS.error)
                 post_result({NAME: ds_name, RESULT: TEMPORARY}, result_queue, log_file_name)
                 return
         time.sleep(FAIL_CONNECTION_WAIT_INTERVALS[tray])
 
+    commands_printout = ""
+    for command in commands:
+        try:
+            commands_printout += connection.send_command(command)
+        except IOError:
+            print_for_ds(ds_name, "Error while execute command {0}".format(command))
+
 
     print_for_ds(ds_name,
-                 '=' * 8 + ' Finish process for {ds} '.format(ds=node.ip) + '=' * 8,
+                 '=' * 8 + ' Finish process.' + '=' * 8,
                  io_lock,
                  log_file_name,
                  COLORS.ok)
-    post_result({NAME: ds_name, RESULT: COMPLETE}, result_queue, log_file_name)
+    post_result({NAME: ds_name, RESULT: COMPLETE, PRINTOUTS: commands_printout}, result_queue, log_file_name)
 
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser(description='Prepare DS upgrade SW to \"{0}\" version.'.format(target_sw_version),
-                                   usage="usage: %prog [-y] [-n] [-l] [-f <DS list file> | ds ds ds ...]")
+    parser = optparse.OptionParser(description='Command execute.',
+                                   usage="usage: %prog [-f <DS list file> | ds ds ds ...] -c command")
     parser.add_option("-f", "--file", dest="ds_list_file_name",
                       help="file with DS list, line started with # or / will be dropped", metavar="FILE")
     parser.add_option("-y", "--yes", dest="force_delete",
@@ -115,7 +159,7 @@ if __name__ == "__main__":
                       help="execute nodes one by one sequentially",
                       action="store_true", default=False)
     parser.add_option("-l", "--log-to-file", dest="log_to_file",
-                      help="enable logging to file yymmdd_hhmmss_ds-name.log",
+                      help="enable logging to file {0}".format(log_file_format),
                       action="store_true", default=False)
     parser.add_option("--no-color", dest="colorize",
                       help="Disable colors",
@@ -146,12 +190,32 @@ if __name__ == "__main__":
         if not is_contains(comment_line_pattern, ds_str) and ds and ds not in ds_list:
             ds_list.append(ds)
 
-    if not ds_list:
+    if not ds_list or len(ds_list) < 1:
+        print(COLORS.error+"No ds found in arguments."+COLORS.end)
         parser.print_help()
         exit()
 
-    if len(ds_list) < 1:
-        print COLORS.error+"No ds found in arguments."+COLORS.end
+    commands_raw = list()
+    if options.commands_str:
+        for command in options.commands_str.split(";"):
+            commands_raw.append(command)
+    if options.command_file:
+        try:
+            with open(options.command_file, "r") as command_file:
+                for command_line in command_file.readlines():
+                    commands_raw.append(command_file)
+        except IOError as e:
+            print(COLORS.error+"Error while open file: {file}".format(file=options.command_file)+COLORS.end)
+            print(str(e))
+
+    commands = list()
+    for command_raw in commands_raw:
+        if not is_contains(comment_line_pattern):
+            commands.append(command_raw)
+
+    if not commands or len(commands) < 1:
+        print(COLORS.error+"Cnn't find command."+COLORS.end)
+        parser.print_help()
         exit()
 
     user = getpass.getuser()
@@ -163,12 +227,14 @@ if __name__ == "__main__":
     print COLORS.info+"Start running: {0}".format(time.strftime("%H:%M:%S"))+COLORS.end
     start_time = time.time()
 
+    result_queue, threads = Queue(), list()
     if len(ds_list) == 1:
-        update_ds(ds_list[0],
-                  user,
-                  secret,
-                  force_delete=options.force_delete,
-                  log_to_file=options.log_to_file)
+        execute_commands(ds_list[0],
+                         user,
+                         secret,
+                         commands,
+                         result_queue,
+                         log_to_file=options.log_to_file)
     else:
         io_lock = threading.Lock()
         result = {COMPLETE: list(), FATAL: list(), TEMPORARY: ds_list}
@@ -176,9 +242,6 @@ if __name__ == "__main__":
         ds_colors = {}
 
         while result[TEMPORARY]:
-
-            result_queue, threads = Queue(), list()
-
             if options.no_threads:
                 handled_ds_count = 0
                 start_tour_time = time.time()
@@ -187,13 +250,13 @@ if __name__ == "__main__":
                     if ds_name not in ds_colors:
                         ds_colors[ds_name] = None
                     try:
-                        update_ds(ds_name,
-                                  user,
-                                  secret,
-                                  result_queue=result_queue,
-                                  force_delete=options.force_delete,
-                                  log_to_file=options.log_to_file,
-                                  color=ds_colors[ds_name])
+                        execute_commands(ds_name,
+                                         user,
+                                         secret,
+                                         commands,
+                                         result_queue,
+                                         log_to_file=options.log_to_file,
+                                         color=ds_colors[ds_name])
                     except Exception as e:
                         print_for_ds(ds_name, "**! Unhandled exception " + str(e), ds_colors[ds_name], COLORS.error)
                         result_queue.put({RESULT: FATAL, NAME: ds_name})
@@ -221,14 +284,15 @@ if __name__ == "__main__":
                         else:
                             ds_colors[ds_name] = None
 
-                    thread = threading.Thread(target=update_ds, name=ds_name, args=(ds_name,
-                                                                                    user,
-                                                                                    secret,
-                                                                                    result_queue,
-                                                                                    io_lock,
-                                                                                    options.force_delete,
-                                                                                    options.log_to_file,
-                                                                                    ds_colors[ds_name]))
+
+                    thread = threading.Thread(target=execute_commands, name=ds_name, args=(ds_name,
+                                                                                           user,
+                                                                                           secret,
+                                                                                           commands,
+                                                                                           result_queue,
+                                                                                           io_lock,
+                                                                                           options.log_to_file,
+                                                                                           ds_colors[ds_name]))
                     thread.start()
                     threads.append(thread)
 
@@ -255,6 +319,11 @@ if __name__ == "__main__":
                     post_result({NAME: ds_name, RESULT: FATAL},
                                 None,
                                 time.strftime(log_file_format.format(ds_name=ds_name)))
+
+            for complete in result[COMPLETE]:
+                print(ds_colors[complete[NAME]]+"\t\tResult for {0}".format(complete[NAME])+COLORS.end)
+                print(ds_colors[complete[NAME]]+complete[PRINTOUTS].format(complete[NAME])+COLORS.end)
+                print(ds_colors[complete[NAME]] + "\t\tFinish for {0}".format(complete[NAME]) + COLORS.end)
 
             if options.colorize and not options.no_threads:
                 line_complete, line_temporary, line_fatal = COLORS.end, COLORS.end, COLORS.end
