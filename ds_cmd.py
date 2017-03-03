42e747ae16b1e72675f4124f4b503896b60e4f3a
Pavel Malko <pavel.malko@kyivstar.ua>
2017-03-03 18:20:35 +0200

    Fix #10

--- ds_cmd.py
+++ ds_cmd.py
@@ -319,9 +319,14 @@ if __name__ == "__main__":
                             time.strftime(log_file_format.format(ds_name=ds_name)))
 
         for ds_complete in result[COMPLETE]:
-            print(ds_colors[ds_complete]+"\t\tResult for {0}".format(ds_complete)+COLORS.end)
-            print(ds_colors[ds_complete]+result[PRINTOUTS][ds_complete].format(ds_complete)+COLORS.end)
-            print(ds_colors[ds_complete] + "\t\tFinish for {0}".format(ds_complete) + COLORS.end)
+            if ds_colors[ds_complete]:
+                print(ds_colors[ds_complete[NAME]]+"\t\tResult for {0}".format(ds_complete[NAME])+COLORS.end)
+                print(ds_colors[ds_complete[NAME]]+result[PRINTOUTS][ds_complete].format(ds_complete[NAME])+COLORS.end)
+                print(ds_colors[ds_complete[NAME]] + "\t\tFinish for {0}".format(ds_complete[NAME]) + COLORS.end)
+            else:
+                print("\t\tResult for {0}".format(ds_complete[NAME]))
+                print(result[PRINTOUTS][ds_complete].format(ds_complete[NAME]))
+                print("\t\tFinish for {0}".format(ds_complete[NAME]))
 
         if options.colorize and not options.no_threads:
             line_complete, line_temporary, line_fatal = COLORS.end, COLORS.end, COLORS.end
